0af92dfccf19893ffdd739bf736127d150b8caf8
Akhmad <akhmadshaleh@gmail.com>
2025-08-27 17:16:46 +0700

    update

--- resources/views/components/payment-status-badge.blade.php
+++ resources/views/components/payment-status-badge.blade.php
@@ -7,11 +7,4 @@
     @else
     <x-badge value="Unpaid" {{ $attributes->merge(['class' => 'text-xs badge-error']) }} />
     @endif
-    {{-- @if ($status == 'paid')
-    <x-badge value="Paid" {{ $attributes->merge(['class' => 'text-xs badge-success']) }} />
-    @elseif ($status == 'outstanding')
-    <x-badge value="Outstanding" {{ $attributes->merge(['class' => 'text-xs badge-warning']) }} />
-    @elseif ($status == 'unpaid')
-    <x-badge value="Unpaid" {{ $attributes->merge(['class' => 'text-xs badge-error']) }} />
-    @endif --}}
 </div>
