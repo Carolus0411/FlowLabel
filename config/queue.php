e2229c1093c9d5fadd95e671322adbc69d3d1d7c
Akhmad <akhmadshaleh@gmail.com>
2026-01-28 20:20:44 +0700

    increase queue retry-after

--- config/queue.php
+++ config/queue.php
@@ -39,7 +39,7 @@ return [
             'connection' => env('DB_QUEUE_CONNECTION'),
             'table' => env('DB_QUEUE_TABLE', 'jobs'),
             'queue' => env('DB_QUEUE', 'default'),
-            'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 5000),
+            'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 11000),
             'after_commit' => false,
         ],
 
